ac2545fb7409b7aede7a20d76e71f63d6e1ee7eb
Pavel <pawel.grackov@gmail.com>
2023-05-15 14:05:44 +0300

    Задача 6

---
+++ src/module1/Task6.java
@@ -0,0 +1,17 @@
+//Задача 1
+package module1;
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите площаль треугольника: ");
+        double area = scanner.nextDouble();
+        System.out.printf("Радиус окружности равен: %.3f", Task6.calcCircleRaduis(area));
+    }
+    public static double calcCircleRaduis(double area){
+        double result = Math.sqrt(area / Math.PI);
+        return result;
+    }
+
+}
