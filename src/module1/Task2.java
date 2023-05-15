ecc37372db10555493c30c8c07c71f4924c4d944
Pavel <pawel.grackov@gmail.com>
2023-05-15 13:30:38 +0300

    Задача 2

---
+++ src/module1/Task2.java
@@ -0,0 +1,18 @@
+package module1;
+import java.util.Scanner;
+
+public class Task2   {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите стоимость товара: ");
+        double price = scanner.nextDouble();
+        System.out.println("Введите количество: ");
+        int count = scanner.nextInt();
+        System.out.println("Итоговая стоимость: " + Task2.priceCalculation(price, count));
+    }
+    public static double priceCalculation(double price, int count){
+        double result = price * count;
+        return result;
+    }
+
+}
