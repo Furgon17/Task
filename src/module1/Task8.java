de8b23af12efebebde96952575a4535d63cc664d
Pavel <pawel.grackov@gmail.com>
2023-05-15 15:34:40 +0300

    Задача 8

---
+++ src/module1/Task8.java
@@ -0,0 +1,16 @@
+package module1;
+import java.util.Scanner;
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите расстоянии от \\ , для вывода символа: ");
+        int a = scanner.nextInt();
+        System.out.println(Task8.charExpression(a));
+
+    }
+    public static char charExpression(int a){
+        int b = '\\'; // используют для получения кода символа
+        char symbol = (char) (b+a);
+        return symbol;
+    }
+}
