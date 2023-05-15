80d26cd0f120eccc1a4c039ce28b762ff043ed03
Pavel <pawel.grackov@gmail.com>
2023-05-15 13:09:43 +0300

    Задача 1

---
+++ src/module1/Task1.java
@@ -0,0 +1,18 @@
+package module1;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество мониторов: ");
+        int monitors = scanner.nextInt();
+        System.out.println("Введите количество программистов: ");
+        int programmers = scanner.nextInt();
+        System.out.println("Столько мониторов никому не достанутся! " + Task1.drawisMonitorsCounter(monitors, programmers));
+    }
+    public static int drawisMonitorsCounter(int monitors, int programmers){
+        int result = monitors % programmers;
+        return result;
+    }
+
+}
