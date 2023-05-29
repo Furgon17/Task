f6502add2949781a547140a2ea36657926bb50c9
Pavel <pawel.grackov@gmail.com>
2023-05-29 22:33:49 +0300

    Задача 7

---
+++ src/Task7/Home/Human.java
@@ -0,0 +1,32 @@
+package Task7.Home;
+
+public class Human {
+    private final String secret; //секретики
+    private final String news; //новости
+    private final String experience; //опыт
+    private final String gossip; //сплетни
+
+    public Human(String secret, String news, String experience, String gossip) {
+        this.secret = secret;
+        this.news = news;
+        this.experience = experience;
+        this.gossip = gossip;
+    }
+
+    private String getSecret() { // доступ только в пределах класса
+        return secret;
+    }
+
+    public String getNews() { // доступ для всех
+        return news;
+    }
+
+    protected String getExperience() { // доступ разрешен только для классов наследников и текущего пакта
+        return experience;
+    }
+
+    String getGossip() { //без модификатора доступа доступ только внутри пакета
+        return gossip;
+    }
+}
+
---
+++ src/Task7/Task7.java
@@ -0,0 +1,17 @@
+package Task7;
+
+import Task7.Home.Human;
+
+public class Task7 extends Human{
+//    Конструктор Task7 вызывает конструктор родительского класса с теми же параметрами, используя ключевое слово super
+    public Task7(String secret, String news, String experience, String gossip) {
+        super(secret, news, experience, gossip);
+    }
+
+    public static void main(String[] args) {
+        Human h = new Human("secret", "news", "experience", "gossip");
+        Task7 t = new Task7("secret", "news", "experience", "gossip");
+        System.out.println(h.getNews() + ", " + t.getExperience()); //secret и gossip тут недоступны,
+        // т.к. имеют приватный и дефолтный модификаторы доступа
+    }
+}
