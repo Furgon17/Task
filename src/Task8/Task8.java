83fccaad72158c75c668c88eb0717d810cef4be8
Pavel <pawel.grackov@gmail.com>
2023-05-29 23:00:21 +0300

    Задача 8

---
+++ src/Task8/Task8.java
@@ -0,0 +1,32 @@
+package Task8;
+
+public class Task8 {
+    public static void main(String[] args) {
+        JavaStudent js = new JavaStudent();
+        Student st = new Student();
+        st.study();
+        js.study();
+    }
+
+    public static class Student {
+        protected final String studying;
+
+        protected Student(String work) {
+            this.studying = work;
+        }
+
+        public Student() {
+            this.studying = "Прохожу тестовое задание.";
+        }
+
+        public void study() {
+            System.out.println("Я очень занят. " + studying);
+        }
+    }
+
+    public static class JavaStudent extends Student {
+        public JavaStudent() {
+            super("Прохожу курсы по Java");
+        }
+    }
+}
