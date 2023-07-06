91d72f7cf4d6393375b7bb33134ae0db8645277a
Pavel <pawel.grackov@gmail.com>
2023-07-06 19:35:22 +0300

    Создание 5 модуля

--- src/module1/Task26.java
+++
@@ -1,7 +0,0 @@
-package module1;
-class Task26 {
-	public static void main(String args[]) {
-		String name = "Pavel";
-		System.out.print(name);
-	}
-}
---
+++ src/module5/Task1.java
@@ -0,0 +1,7 @@
+package module5;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
